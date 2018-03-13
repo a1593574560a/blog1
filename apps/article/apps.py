7e40cf583ee58bba7341648ce289cbfde8852aba
a1593574560a <1156982879@qq.com>
2018-03-13 11:05:55 +0800

    Revert "Initial commit"
    This reverts commit 494d4389d6c172355c4d71afaebdd6c7ec739b73.

--- apps/article/apps.py
+++
@@ -1,7 +0,0 @@
-#_*_ encoding: utf-8 _*_
-from django.apps import AppConfig
-
-
-class ArticleConfig(AppConfig):
-    name = 'article'
-    verbose_name = u'文章管理'
--- extra_app/__init__.py
+++
@@ -1,3 +0,0 @@
-#_*_ encoding: utf-8 _*_
-__author__ = "Administrator"
-__date__ = "2018/1/30 , 15:13"
